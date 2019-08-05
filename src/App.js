75855b2472ff22c65f0e017356949acc67e3cb25
Igor Souza <igormcsouza@gmail.com>
2019-08-05 20:12:22 -0300

    created react application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.css'
+import Header from './components/header'
+
+const App = () => (
+    <div className="App">
+      <Header />
+    </div>
+)
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.css'
+
+const Header = () => (
+    <header id='main-header'> Online Classes </header>
+)
+
+export default Header
