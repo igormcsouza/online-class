efdf79cfa0004b9d6eb81d743e93187faae057ef
Igor Souza <igormcsouza@gmail.com>
2019-08-06 19:39:30 -0300

    created the actual app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LandingPage from './landing-page'
+// import Application from './app'
 
 const App = () => (
     <div className="App">
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Application = () => (
+    <div className="Application">
+      <p> This is an App! </p>
+    </div>
+)
+
+export default Application;
