46a8052f96c1f72d2347706da12fa807800c7b8c
Igor Souza <igormcsouza@gmail.com>
2019-08-05 22:26:03 -0300

    fixed headers

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 import './styles.css'
 
 const Header = () => (
-    <header id='main-header'>Aulas de Inglês sem Sair de Casa</header>
+    <div id='bg-header'>
+        <header id='main-header'>
+            <p>Aulas de Inglês sem Sair de Casa</p>
+        </header>
+    </div>
 )
 
 export default Header
