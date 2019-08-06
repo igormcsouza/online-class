ad959256a813c823c1dfe5902a52a8e9dc2c8c59
Igor Souza <igormcsouza@gmail.com>
2019-08-06 19:33:01 -0300

    creating footer

---
+++ src/landing-page/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles.css'
+
+const Footer = () => (
+    <footer id='main-footer'>
+        Igor Souza - Professor de Inglês Particular
+    </footer>
+)
+
+export default Footer
